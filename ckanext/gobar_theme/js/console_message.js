471c58de2cba41c2a2603ed471572a525b27b04e
Martin Szyszlican <mszyszlican@JUSTICIA.AR>
2017-12-07 15:55:39 -0300

    Correcciones de textos en encabezados, titulos y metadatos

--- ckanext/gobar_theme/js/console_message.js
+++ ckanext/gobar_theme/js/console_message.js
@@ -2,7 +2,7 @@ $(function () {
     var hello_message = '%c\n██╗  ██╗ ██████╗ ██╗      █████╗ \n██║  ██║██╔═══██╗██║     ██╔══██╗\n███████║██║   ██║██║     ███████║\n██╔══██║██║   ██║██║     ██╔══██║\n██║  ██║╚██████╔╝███████╗██║  ██║\n╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝';
     var hello_styles = 'font-size: 10px;color:black;font-family:monospace;font-weight:bold;';
     console.log(hello_message, hello_styles);
-    var contact_message = '%c\nGracias por visitar nuestro portal (:\nPuedes enviar cualquier comentario a gobiernoabierto@modernizacion.gob.ar y ver el codigo de esta web en https://github.com/gobabiertoAR/datos.gob.ar';
+    var contact_message = '%c\n¿Podemos ayudarte en algo? (:\nEste Portal esttá desarrollado por el Programa Justicia Abierta. Usamos CKAN.\n Para más detalles te invitamos a mirar el Github del Programa: https://github.com/datos-justicia-argentina/datos.jus.gob.ar \n Nuesta dirección de correo es justiciaabierta@jus.gob.ar  ';
     var contact_styles = 'font-size: 16px;color:black;font-family:monospace';
     console.log(contact_message, contact_styles);
 });
